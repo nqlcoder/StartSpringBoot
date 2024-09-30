44d9960b85f07125783b1a9127521b24b169216f
ling <nqlcoder@gmail.com>
2024-09-30 21:54:44 +0700

    Quản lý Exception và trả về Response với HttpCode tùy biến

--- src/main/java/com/lingg/hellospringboot/exception/GlobalExceptionHandler.java
+++ src/main/java/com/lingg/hellospringboot/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.lingg.hellospringboot.exception;
 
 import com.lingg.hellospringboot.dto.request.ApiResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -23,7 +24,20 @@ public class GlobalExceptionHandler {
         ErrorCode errorCode = exception.getErrorCode();
         apiResponse.setCode(errorCode.getCode());
         apiResponse.setMessage(errorCode.getMessage());
-        return ResponseEntity.badRequest().body(apiResponse);
+        return ResponseEntity
+                .status(errorCode.getStatusCode())
+                .body(apiResponse);
+    }
+
+    @ExceptionHandler(value = AccessDeniedException.class)
+    ResponseEntity<ApiResponse> handlingAccessDeniedException(AccessDeniedException exception) {
+        ErrorCode errorCode = ErrorCode.UNAUTHORIZED;
+        return ResponseEntity
+                .status(errorCode.getStatusCode())
+                .body(ApiResponse.builder()
+                        .code(errorCode.getCode())
+                        .message(errorCode.getMessage())
+                        .build());
     }
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
