0d3777deff0539b0d3c36eaa9ca030bf8fc7b5f0
ling <nqlcoder@gmail.com>
2024-11-28 22:19:39 +0700

    #21 code coverage với JaCoCo và cách tăng code coverage

--- src/test/java/com/lingg/hellospringboot/services/UserServiceTest.java
+++ src/test/java/com/lingg/hellospringboot/services/UserServiceTest.java
@@ -5,21 +5,24 @@ import com.lingg.hellospringboot.dto.response.UserResponse;
 import com.lingg.hellospringboot.entity.User;
 import com.lingg.hellospringboot.exception.AppException;
 import com.lingg.hellospringboot.repositories.IUserRepository;
+
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.TestPropertySource;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @TestPropertySource("/test.properties")
@@ -63,10 +66,10 @@ public class UserServiceTest {
     }
 
     @Test
-    void create_validRequest_success(){
+    void create_validRequest_success() {
         // GIVEN
-        Mockito.when(repository.existsByUsername(anyString())).thenReturn(false);
-        Mockito.when(repository.save(any())).thenReturn(user);
+        when(repository.existsByUsername(anyString())).thenReturn(false);
+        when(repository.save(any())).thenReturn(user);
 
         // WHEN
         var response = userService.createUser(request);
@@ -77,14 +80,33 @@ public class UserServiceTest {
     }
 
     @Test
-    void create_userExisted_fail(){
+    void create_userExisted_fail() {
         // GIVEN
-        Mockito.when(repository.existsByUsername(anyString())).thenReturn(true);
+        when(repository.existsByUsername(anyString())).thenReturn(true);
 
         // WHEN
         var exception = assertThrows(AppException.class, () -> userService.createUser(request));
 
         // THEN
         Assertions.assertThat(exception.getErrorCode().getCode()).isEqualTo(1002);
+    }
+
+    @Test
+    @WithMockUser(username = "nql1")
+    void getMyInfo_valid_success() {
+        when(repository.findByUsername(anyString())).thenReturn(Optional.of(user));
+
+        var response = userService.getMyInfo();
+        Assertions.assertThat(response.getId()).isEqualTo("abc12345");
+        Assertions.assertThat(response.getUsername()).isEqualTo("nql");
+    }
+
+    @Test
+    @WithMockUser(username = "nql1")
+    void getMyInfo_userNotFound_error() {
+        when(repository.findByUsername(anyString())).thenReturn(Optional.ofNullable(null));
+
+        var exception = assertThrows(AppException.class, () -> userService.getMyInfo());
+        Assertions.assertThat(exception.getErrorCode().getCode()).isEqualTo(1005);
     }
 }
